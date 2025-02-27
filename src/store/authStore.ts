c007596ddeca4f48c4235d8a5b51760db085d5bf
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2025-02-27 13:33:06 +0530

    refactor

--- src/store/authStore.ts
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-import { AuthState } from '../types';
-
-export const useAuthStore = create<AuthState>((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
-}));
